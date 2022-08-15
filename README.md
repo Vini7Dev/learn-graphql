35ecf1dd50219c1c2116be7dc3ed2d71531f3bf8
Vinicius7Codeby <viniciusgabriel.dev@gmail.com>
2022-08-15 08:16:04 -0300

    feat: scalar types

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # GraphQL Course
 
 - Link to [Course](https://www.udemy.com/course/curso-de-graphql-e-apollo-server-client)
+
+# Notes
+
+# Scalar Types
+
+**Text Types:**
+- ID
+- String
+
+**Number Types:**
+- Int
+- Float
+
+**Logical Types:**
+- Boolean
+
+**Array Types:**
+- [Type]
+  *Example: [Int]*
+
+**Handling Response:**
+- field: Type! (Not null)
+  *Example: name: String!*
+- field: [Type!]! (Not null and no elements null)
--- class-01/src/index.ts
+++ class-01/src/index.ts
@@ -1,1 +1,35 @@
-console.log('Hello World!');
+import { ApolloServer, gql } from 'apollo-server';
+
+const SERVER_PORT = 4003;
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      id: ID
+      name: String!
+      age: Int
+      hight: Float
+      married: Boolean
+      pointsArray: [Int!]!
+    }
+  `,
+  resolvers: {
+    Query: {
+      id: () => 'g4fd34354',
+      name: () => 'John Doe',
+      age: () => 20,
+      hight: () => 1.71,
+      married: () => false,
+      pointsArray: () => [10, 8, 9],
+    },
+  },
+});
+
+server
+  .listen(SERVER_PORT)
+  .then(({ url }) => {
+    console.log(`===> Server listening on url ${url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
